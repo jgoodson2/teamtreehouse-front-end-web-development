51885403edd131b7f4a54ad533a7bd887c7c9fe3
Jonathon Goodson <goodson.jonathon@gmail.com>
2015-05-26 22:12:51 -0400

    began creating a spoiler revealer project by hiding spoiler text and creating a ;reveal spoiler' button

---
+++ jquery-basics/creating-a-spoiler-revealer/js/app.js
@@ -0,0 +1,10 @@
+//Prevent spoilers from showing
+//Solution: Hide spoilers and present them thru user interaction
+
+//1. Hide spoiler
+$(".spoiler span").hide();
+//2. Add a button
+$(".spoiler").append("<button>reveal spoiler!!!</button>");
+//3. When button pressed,
+////A. Show spoiler
+////B. Get rid of button
