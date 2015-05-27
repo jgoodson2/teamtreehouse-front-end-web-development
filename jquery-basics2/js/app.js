ebfe1b519a9f0f45170519040559043acd677d42
Jonathon Goodson <goodson.jonathon@gmail.com>
2015-05-26 20:03:59 -0400

    Explored different places to insert jquery reference insertion in html; explored different ways of handling 'On Ready' load event

--- jquery-basics2/js/app.js
+++ jquery-basics2/js/app.js
@@ -1,3 +1,16 @@
 function myCode() {
-	$(".warning").hide().show("slow");
+    $(".warning").hide().show("slow");
 }
+
+//$(document).ready(myCode);
+//$(myCode);
+
+//$(document).ready(function () {
+//    $(".warning").hide().show("slow");
+//});
+
+//$(function () {
+//    $(".warning").hide().show("slow");
+//});
+
+$(".warning").hide().show("slow");
