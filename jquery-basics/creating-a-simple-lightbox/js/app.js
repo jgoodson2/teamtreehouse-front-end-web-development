629a8ab3fb91ba66310a32002f316e79cfe9ed32
Jonathon Goodson <goodson.jonathon@gmail.com>
2015-05-29 19:09:14 -0400

    in jquery-basics/creating-a-simple-lightbox/js/app.js, created var  which is disembodied <p> tag and appended to ; also extracted text from the img alt attr and made this text the content of the caption of ; styled appropriately

--- jquery-basics/creating-a-simple-lightbox/js/app.js
+++ jquery-basics/creating-a-simple-lightbox/js/app.js
@@ -4,23 +4,35 @@
 
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
+var $caption =$("<p></p>");
 
 $overlay.append($image);
+$overlay.append($caption);
 
-//0 Add an overlay
+
+// Add an overlay
 $("body").append($overlay);
 //0.1 an image
 //0.2 a caption
 
-//1. capture the click event on a link to an image
+// capture the click event on a link to an image
 $("#imageGallery a").click(function(event){
     event.preventDefault();
-    var href = $(this).attr("href");
+    var imageLocation = $(this).attr("href");
+    var imageAlt = $(this).children("img").attr("alt");
     //console.log(href);
+    // update overlay to the image linked in the link
+    $image.attr("src", imageLocation);
+    // Get child's alt attribute and set caption
+    $image.attr("alt", imageAlt);
+
+    $caption.text(imageAlt);
+
+    // show the overlay
     $overlay.show();
-  //1.1 show the overlay
-  //1.2 update overlay to the image linked in the link
-  //1.3 Get child's alt attribute and set caption
 });
-//3. when overlay is clicked
-  //3.1 hide overlay
+// when overlay is clicked
+$overlay.click(function(){
+    // hide overlay
+    $(this).hide();
+});
