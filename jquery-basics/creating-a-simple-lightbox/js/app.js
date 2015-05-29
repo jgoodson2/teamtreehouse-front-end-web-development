b253777d45ca17d11815fd46c45e2b7fe7e4e272
Jonathon Goodson <goodson.jonathon@gmail.com>
2015-05-28 22:10:40 -0400

    used jquery to append div element with id=overly to body node of index.html; styled new div with css so that it will 'darken' everything on click in creating-a-simple-lightbox/css/style.css

--- jquery-basics/creating-a-simple-lightbox/js/app.js
+++ jquery-basics/creating-a-simple-lightbox/js/app.js
@@ -2,17 +2,22 @@
 
 //Solution: create an overlay with large image - Lightbox
 
+var $overlay = $('<div id="overlay"></div>');
+
+//0 Add an overlay
+$("body").append($overlay);
+//0.1 an image
+//0.2 a caption
+
 //1. capture the click event on a link to an image
 $("#imageGallery a").click(function(event){
     event.preventDefault();
     var href = $(this).attr("href");
-    console.log(href);
+    //console.log(href);
+    $overlay.show();
   //1.1 show the overlay
   //1.2 update overlay to the image linked in the link
   //1.3 Get child's alt attribute and set caption
 });
-//2. Add an overlay
-  //2.1 an image
-  //2.2 a caption
 //3. when overlay is clicked
   //3.1 hide overlay
