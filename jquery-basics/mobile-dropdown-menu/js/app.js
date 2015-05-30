1a9d1b371bd5b82b76c2af4587919f02ea756328
Jonathon Goodson <goodson.jonathon@gmail.com>
2015-05-30 08:36:01 -0400

    added pseudocode to jquery-basics/mobile-dropdown-menu/js/app.js to plan out all steps

---
+++ jquery-basics/mobile-dropdown-menu/js/app.js
@@ -0,0 +1,15 @@
+//Problem: it looks gross in smaller browser widths and small devices
+//Solution: to hide the text links and swap them out with a more appropriate navigation\
+
+//Create a select and append to menu
+//Cycle over menu links
+  //Create an option
+  //options value is the href of link
+  //optiosn text is text of link
+  //append option to select
+//Create button to click to go to select's location
+  //bind click action to button
+  //go to select's location
+//Modify CSS to hide links on small widths and show button and select
+//Also hides select and button on larger widths and shows links
+//deal with selected options depending on current page
