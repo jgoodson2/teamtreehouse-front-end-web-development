8619eb7569835a9483251df785e60a96acb9bd0d
jgoodson2 <jonathongoodson@clayton.edu>
2015-06-04 22:28:45 -0400

    in mobile-dropdown-menu/app.js, changed jquery to add 'value' attributes instead of href attributes, which was wrong

--- jquery-basics/mobile-dropdown-menu/js/app.js
+++ jquery-basics/mobile-dropdown-menu/js/app.js
@@ -13,15 +13,20 @@ $("#menu a").each(function(index){
     //Create an option
     var $option = $("<option></option>");
     //option's value is the href of link
-    $option.attr("href", $anchor.attr("href"));
+    $option.val($anchor.attr("href"));
     //option's text is text of link
     $option.text($anchor.text());
     //append option to select
     $select.append($option);
 });
 //Create button to click to go to select's location
+var $button = $("<button>Go!</button>");
+$("#menu").append($button);
   //bind click action to button
-  //go to select's location
+$button.click(function(){
+    //go to select's location
+
+});
 //Modify CSS to hide links on small widths and show button and select
 //Also hides select and button on larger widths and shows links
 //deal with selected options depending on current page
