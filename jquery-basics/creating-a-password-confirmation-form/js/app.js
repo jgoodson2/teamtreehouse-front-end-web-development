d06b5dc6750f8e5bbf9e582686a46090c8beadd2
jgoodson2 <jonathongoodson@clayton.edu>
2015-06-21 18:44:44 -0400

    in creating-a-password-confirmation-form, in js/app.js added code that will hide and show hint based on minimum password length

--- jquery-basics/creating-a-password-confirmation-form/js/app.js
+++ jquery-basics/creating-a-password-confirmation-form/js/app.js
@@ -2,12 +2,23 @@
 //Solution: Hide and show them at appropriate times
 
 //hide hints
+$("form span").hide();
 
-//find out if PW is valid
-  //hide hint if valid
-  //else show hint
+function passwordEvent() {
+    ///find out if PW is valid
+    if ($("#password").val().length > 8) {
+        ////hide hint if valid
+        $(this).next().hide();
+    }
+    ////else show hint
+    else {
+        $(this).next().show();
+    }
+}
+
+$("#password").focus(passwordEvent).keyup(passwordEvent);
 
 //when event happens on confirmation input
-  //Find out if password and confirmation match
-    //Hide hint if matched
-    //else show hint
+///Find out if password and confirmation match
+////Hide hint if matched
+////else show hint
