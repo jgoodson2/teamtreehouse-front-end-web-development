adf6ba19e79301417bdf6d4c75ed1266dc158ff6
jgoodson2 <jonathongoodson@clayton.edu>
2015-06-21 16:50:07 -0400

    in creating-a-password-confirmation-form, added pseudocoded plan into js/app.js

---
+++ jquery-basics/creating-a-password-confirmation-form/js/app.js
@@ -0,0 +1,13 @@
+//Problem: Hints are shown even when form is valid
+//Solution: Hide and show them at appropriate times
+
+//hide hints
+
+//find out if PW is valid
+  //hide hint if valid
+  //else show hint
+
+//when event happens on confirmation input
+  //Find out if password and confirmation match
+    //Hide hint if matched
+    //else show hint
