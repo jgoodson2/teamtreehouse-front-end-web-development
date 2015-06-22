da3ad3eccee3a775d43e5e324ac90b1e389a1d42
Jonathon Goodson <goodson.jonathon@gmail.com>
2015-06-22 11:54:03 -0400

    in creating-a-pw project, in app.js, added confirm password validation

--- jquery-basics/creating-a-password-confirmation-form/js/app.js
+++ jquery-basics/creating-a-password-confirmation-form/js/app.js
@@ -1,24 +1,36 @@
 //Problem: Hints are shown even when form is valid
 //Solution: Hide and show them at appropriate times
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
 
 //hide hints
 $("form span").hide();
 
 function passwordEvent() {
     ///find out if PW is valid
-    if ($("#password").val().length > 8) {
+    if ($password.val().length > 8) {
         ////hide hint if valid
-        $(this).next().hide();
+        $password.next().hide();
     }
     ////else show hint
     else {
-        $(this).next().show();
+        $password.next().show();
     }
 }
 
-$("#password").focus(passwordEvent).keyup(passwordEvent);
+function confirmPasswordEvent() {
+///Find out if password and confirmation match
+    if ($password.val() === $confirmPassword.val()) {
+////Hide hint if matched
+        $confirmPassword.next().hide();
+    }
+////else show hint
+    else {
+        $confirmPassword.next().show();
+    }
+}
+
+$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
 
 //when event happens on confirmation input
-///Find out if password and confirmation match
-////Hide hint if matched
-////else show hint
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
