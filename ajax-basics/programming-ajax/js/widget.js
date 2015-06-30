64a97f1bbb8dbc166eff37b81635dffb7e046d0d
jgoodson2 <jonathongoodson@clayton.edu>
2015-06-29 22:16:05 -0400

    Added starter code for porgramming-ajax/stage-2-challenge

--- ajax-basics/programming-ajax/js/widget.js
+++ ajax-basics/programming-ajax/js/widget.js
@@ -1,11 +1,19 @@
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
         //console.log(xhr.responseText);
         var employees = JSON.parse(xhr.responseText);
         //console.log(employees);
+        var statusHTML = '<ul class="bulleted">';
+        for (var i = 0; i < employees.length; i += 1) {
+            statusHTML += '<li class=';
+            statusHTML += employees[i].inoffice==true?'in':'out';
+            statusHTML += '>' + employees[i].name + '</li>';
+        }
+        statusHTML += '</ul>';
+        document.getElementById('employeeList').innerHTML = statusHTML;
     }
 };
-xhr.open('GET','data/employees.json');
+xhr.open('GET', 'data/employees.json');
 xhr.send();
 
