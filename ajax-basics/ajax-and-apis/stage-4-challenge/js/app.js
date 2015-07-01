4b65bd91d85f3cf54acb5e018b0c299b59834f7c
Jonathon Goodson <goodson.jonathon@gmail.com>
2015-07-01 11:19:26 -0400

    In ajax-and-apis/stage-4-challenge, added code that will retrieve and display photos based on the user's search term

--- ajax-basics/ajax-and-apis/stage-4-challenge/js/app.js
+++ ajax-basics/ajax-and-apis/stage-4-challenge/js/app.js
@@ -1,31 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    $('form').submit(function (evt) {
+        evt.preventDefault();
+        var searchTerm = $('#search').val();
+        var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        var flickrOptions = {
+            tags: searchTerm,
+            format: "json"
+        };
 
+        function displayPhotos(data) {
+            var photoHTML = '<ul>';
+            $.each(data.items, function (i, photo) {
+                photoHTML += '<li class="grid-25 table-grid-50">';
+                photoHTML += '<a href="' + photo.link + '" class="image">';
+                photoHTML += '<img src="' + photo.media.m + '">';
+                photoHTML += '</a>';
+                photoHTML += "</li>";
+            });
+            photoHTML += '</ul>';
+            $("#photos").html(photoHTML);
+        }
 
- $('button').click(function () {
-    // highlight the button
-    // not AJAX, just cool looking
-    $("button").removeClass("selected");
-    $(this).addClass("selected");
+        $.getJSON(flickrAPI, flickrOptions, displayPhotos);
 
-    // the AJAX part
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var animal = $(this).text();
-    var flickrOptions = {
-      tags: animal,
-      format: "json"
-    };
-    function displayPhotos(data) {
-      var photoHTML = '<ul>';
-      $.each(data.items,function(i,photo) {
-        photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-      }); // end each
-      photoHTML += '</ul>';
-      $('#photos').html(photoHTML);
-    }
-    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
-
-  }); // end click
-
-}); // end ready
+    });
+});
